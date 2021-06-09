e88650997d1b13352580a51b25d978bd7dfe3d00
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-06-09 18:59:55 +0100

    Integrate Realtime service with pusher

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :username, presence: :true, uniqueness: { case_sensitive: false }
   validates :is_signed_in, inclusion: [true, false]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  after_create :notify_pusher # add this line
+
+  def notify_pusher
+    Pusher.trigger('activity', 'login', self.as_json)
+  end
+
+  def as_json(options={})
+    super(
+      only: [:id, :email, :username]
+    )
+  end
 end
